c153f242cba53e189064c315c2d8e82cd1fd4fb8
AbhishekApp <abhishekk@onsinteractive.com>
2016-08-05 20:18:02 +0530

    Chat page tabs are created

--- app/src/main/java/appy/com/wazznowapp/EventChatFragment.java
+++ app/src/main/java/appy/com/wazznowapp/EventChatFragment.java
@@ -28,6 +28,7 @@ public class EventChatFragment extends AppCompatActivity {
     private ViewPager viewPager;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/appy/com/wazznowapp/StadiumFragment.java
+++ app/src/main/java/appy/com/wazznowapp/StadiumFragment.java
@@ -1,15 +1,44 @@
 package appy.com.wazznowapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by admin on 8/2/2016.
  */
-public class StadiumFragment extends Fragment {
+public class StadiumFragment extends Fragment implements View.OnClickListener {
+
+    ListView listView;
+    ImageView imgEmoji;
+    ImageView send;
+    ArrayList<String> al;
+    ArrayAdapter<String> adapter;
+    EditText etMsg;
+
+    String mUsername;
+    String mPhotoUrl;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+
 
     public StadiumFragment(){
 
@@ -17,14 +46,70 @@ public class StadiumFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+                super.onCreate(savedInstanceState);
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE | WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.my_list, container, false);
+        View view = inflater.inflate(R.layout.stadium_chat, container, false);
+        try{
+            mFirebaseAuth = FirebaseAuth.getInstance();
+            mFirebaseUser = mFirebaseAuth.getCurrentUser();
 
+            if (mFirebaseUser == null) {
+                // Not signed in, launch the Sign In activity
+                startActivity(new Intent(getActivity(), SignUpActivity.class));
+                getActivity().finish();
+//                return;
+            } else {
+                mUsername = mFirebaseUser.getDisplayName();
+                if (mFirebaseUser.getPhotoUrl() != null) {
+                    mPhotoUrl = mFirebaseUser.getPhotoUrl().toString();
+                }
+            }
+
+        }catch (Exception ex){}
+
+        init(view);
+
+        return view;
+
+    }
+
+    private void init(View v){
+        listView = (ListView) v.findViewById(R.id.listMain);
+        imgEmoji = (ImageView) v.findViewById(R.id.imgEmoji);
+        send = (ImageView) v.findViewById(R.id.imgSendChat);
+        etMsg = (EditText) v.findViewById(R.id.etChatMsg);
+        listView.setTranscriptMode(AbsListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+
+        al = new ArrayList<String>();
+
+        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.activity_list_item, android.R.id.text1, al);
+        listView.setAdapter(adapter);
+        imgEmoji.setOnClickListener(this);
+        send.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id == R.id.imgEmoji){
+
+        }
+        else if(id == R.id.imgSendChat){
+            String msg = etMsg.getText().toString();
+            if(!TextUtils.isEmpty(msg)) {
+                al.add(msg);
+                adapter.notifyDataSetChanged();
+                etMsg.setText("");
+            }else{
+                Toast.makeText(getActivity(), "There is not any message.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
