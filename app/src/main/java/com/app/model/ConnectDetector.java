08a5a2768929992ae68f5a8f0bc5e35f9b0197ae
AbhishekApp <abhishekk@onsinteractive.com>
2016-08-11 20:04:30 +0530

    Chat page tabs are created

---
+++ app/src/main/java/com/app/model/ConnectDetector.java
@@ -0,0 +1,24 @@
+package com.app.model;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by admin on 8/11/2016.
+ */
+public class ConnectDetector {
+
+    Context con;
+
+    public ConnectDetector(Context context){
+        con = context;
+    }
+
+    public boolean getConnection(){
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) con.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+}
