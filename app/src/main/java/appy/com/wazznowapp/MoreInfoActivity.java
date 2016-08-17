c16e2dea35b8ed743e924b4cadd814bc017670e7
AbhishekApp <abhishekk@onsinteractive.com>
2016-08-17 19:55:00 +0530

    Chat page tabs are created

---
+++ app/src/main/java/appy/com/wazznowapp/MoreInfoActivity.java
@@ -0,0 +1,50 @@
+package appy.com.wazznowapp;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+/**
+ * Created by admin on 8/17/2016.
+ */
+public class MoreInfoActivity extends AppCompatActivity {
+
+    ActionBar actionBar;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        setContentView(R.layout.more_info_activity);
+
+
+        init();
+    }
+
+    private void init(){
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+//        actionBar.setDisplayShowHomeEnabled(true);
+       // actionBar.setTitle("More Info");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater menuInflater = getMenuInflater();
+//        menuInflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
