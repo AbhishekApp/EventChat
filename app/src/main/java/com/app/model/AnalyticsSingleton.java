05f0a57aef7958ccbf99a2745609abecb02b535a
Manish <manish@onsinteractive.com>
2017-02-01 17:10:21 +0530

    bug fixing & analytics implementation

---
+++ app/src/main/java/com/app/model/AnalyticsSingleton.java
@@ -0,0 +1,27 @@
+package com.app.model;
+
+/**
+ * Created by admin on 1/27/2017.
+ */
+
+public class AnalyticsSingleton {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
