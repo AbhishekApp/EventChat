62b72c0457a3d015e0e1ffd995b582c0ddffabaa
Manish <manish@onsinteractive.com>
2017-03-11 19:52:18 +0530

    resolved bugs.

---
+++ app/src/main/java/com/app/model/CannedFootballMessage.java
@@ -0,0 +1,18 @@
+package com.app.model;
+
+/**
+ * Created by admin on 11/3/2017.
+ */
+
+public class CannedFootballMessage {
+    String canned_message;
+
+    public String getCanned_message() {
+        return canned_message;
+    }
+
+    public void setCanned_message(String canned_message) {
+        this.canned_message = canned_message;
+    }
+}
+
